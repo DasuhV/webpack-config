0e31e74bab5a54e53ce226c651d7f1117f614a08
DanielLu <lushnikovdaniel2709@gmail.com>
2023-12-20 17:50:18 +0300

    sourse map helps us to fix bugs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,8 +7,14 @@ import Nike from '@/assets/nike.svg'
 import snail from '@/assets/snail.png'
 //style
 import styles from './App.module.scss'
-
+const TODO = () => {
+	TODO4()
+}
+const TODO4 = () => {
+	throw new Error('TODO4')
+}
 const App = () => {
+
 	const [count, setCount] = useState(0)
 	__PLATFORM__ === 'desktop'
 		? console.log(`this is desktop babe `)
@@ -19,18 +25,20 @@ const App = () => {
 	return (
 		<div data-testId={'App.dataTestId'}>
 			<h1 data-testId={'PlatformName.dataTestId'}>
-				__PLATFORM__:{__PLATFORM__}
+				__PLATFORM__:{__PLATFORM__} {' '}
+				__MODE__:{__MODE__}
 			</h1>
-			<Link to={'/about'}>{<img src={snail} alt='' style={{ width: 100 }} />}</Link>
+			<Link to={'/about'}>
+				{<img src={snail} alt='' style={{ width: 100 }} />}
+			</Link>
 			<Link to={'/dog'}>{<img src={dog} alt='' style={{ width: 100 }} />}</Link>
 			<br />
-			<Link to={'/shop'}><Nike fill='green' width={100} height={100} /></Link>
+			<Link to={'/shop'}>
+				<Nike fill='green' width={100} height={100} />
+			</Link>
 			<h1>{count}</h1>
 			<div className={styles.buttonGroup}>
-				<button
-					className={styles.button}
-					onClick={() => setCount(prev => prev + 1)}
-				>
+				<button className={styles.button} onClick={() => TODO()}>
 					increment
 				</button>
 			</div>
